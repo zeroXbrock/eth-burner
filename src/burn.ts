cadcd127e501638aca23aeddb3e50b288364b81b
brock <brocksmedley@gmail.com>
2021-12-06 12:45:20 -0800

    fetch nonce manually, and as late as possible

--- src/burn.ts
+++ src/burn.ts
@@ -4,28 +4,30 @@ const { formatEther } = utils;
 
 const burn = async (burnWallet: Wallet) => {
     const balance = await burnWallet.getBalance();
-        if (balance.isZero()) {
-            console.log(`==== Balance is zero`);
-            return;
-        }
+    const nonce = await burnWallet.provider.getTransactionCount(burnWallet.address);
+    if (balance.isZero()) {
+        console.log(`Balance is zero`);
+        return;
+    }
 
-        const gasPrice = balance.div(21000);
-        if (gasPrice.lt(1e9)) {
-            console.log(` Balance too low to burn (balance=${formatEther(balance)} gasPrice=${gasPriceToGwei(gasPrice)})`);
-            return;
-        }
+    const gasPrice = balance.div(21000);
+    if (gasPrice.lt(1e9)) {
+        console.log(`Balance too low to burn (balance=${formatEther(balance)} gasPrice=${gasPriceToGwei(gasPrice)})`);
+        return;
+    }
 
-        try {
-            console.log(` Burning ${formatEther(balance)}`);
-            const tx = await burnWallet.sendTransaction({
-                to: burnWallet.address,
-                gasLimit: 21000,
-                gasPrice,
-            });
-            console.log(` Sent tx with nonce ${tx.nonce} burning ${formatEther(balance)} ETH at gas price ${gasPriceToGwei(gasPrice)}`);
-        } catch (err: any) {
-            console.log(` Error sending tx: ${err.message ?? err}`);
-        }
+    try {
+        console.log(`Burning ${formatEther(balance)}`);
+        const tx = await burnWallet.sendTransaction({
+            to: burnWallet.address,
+            gasLimit: 21000,
+            gasPrice,
+            nonce
+        });
+        console.log(`Sent tx with nonce ${tx.nonce} burning ${formatEther(balance)} ETH at gas price ${gasPriceToGwei(gasPrice)}`);
+    } catch (err: any) {
+        console.log(`Error sending tx: ${err.message ?? err}`);
+    }
 }
 
 export default burn;
