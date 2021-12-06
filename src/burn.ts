3592287c34151e413417fcc31a93174a43028956
brock <brocksmedley@gmail.com>
2021-12-06 13:26:16 -0800

    remove unnecessary utils invocations

--- src/burn.ts
+++ src/burn.ts
@@ -17,7 +17,7 @@ const burn = async (burnWallet: Wallet) => {
         return;
     }
     const leftovers = balance.sub(gasPrice.mul(21000));
-    console.log(`Leftovers: ${utils.formatEther(leftovers)} ETH`);
+    console.log(`Leftovers: ${formatEther(leftovers)} ETH`);
 
     try {
         console.log(`Burning ${formatEther(balance)}`);
@@ -30,7 +30,7 @@ const burn = async (burnWallet: Wallet) => {
             value: leftovers,
         });
         console.log(`Sent tx with nonce ${tx.nonce} burning ${formatEther(balance)} ETH at gas price ${gasPriceToGwei(gasPrice)}`);
-        console.log(`Beer fund balance: ${flashbotsBeerFund && utils.formatEther(await burnWallet.provider.getBalance(flashbotsBeerFund))} ETH`);
+        console.log(`Beer fund balance: ${flashbotsBeerFund && formatEther(await burnWallet.provider.getBalance(flashbotsBeerFund))} ETH`);
     } catch (err: any) {
         console.log(`Error sending tx: ${err.message ?? err}`);
     }
