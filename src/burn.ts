c0a2816d2a662a08c4023acbb32accc41c44f4df
brock <brocksmedley@gmail.com>
2021-12-06 12:46:40 -0800

    add 'gwei' to log output

--- src/burn.ts
+++ src/burn.ts
@@ -11,7 +11,7 @@ const burn = async (burnWallet: Wallet) => {
 
     const gasPrice = balance.div(21000);
     if (gasPrice.lt(1e9)) {
-        console.log(`Balance too low to burn (balance=${formatEther(balance)} gasPrice=${gasPriceToGwei(gasPrice)})`);
+        console.log(`Balance too low to burn (balance=${formatEther(balance)} gasPrice=${gasPriceToGwei(gasPrice)}) gwei`);
         return;
     }
 
